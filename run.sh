148ead6e5051fcdf4bda3ccc7eb26c9531aae9bd
ChenYi·Li <lichenyiwin@outlook.com>
2024-07-12 13:08:11 +0800

    最新脚本

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 # 服务配置
 SERVICE_CMD="python SocketServer.py --chatVer 3 --APIKey [] --brainwash False --model gpt-3.5-turbo --stream False --character paimon"
 SERVICE_PORT=8800
-RESTART_INTERVAL=1800  # 100分钟，单位：秒  6000-100分钟 1800-30分钟  300-5分钟
 
 # 检查端口是否被占用并返回对应的PID
 get_port_pid() {
@@ -15,20 +14,20 @@ start_service() {
   while true; do
     PID=$(get_port_pid)
     if [ -z "$PID" ]; then
-      echo "Starting service on port ${SERVICE_PORT}..."
+      echo "*** Starting service on port ${SERVICE_PORT}..."
       $SERVICE_CMD &
       SERVICE_PID=$!
-      echo "Service started with PID ${SERVICE_PID}. Waiting for 30 seconds..."
-      sleep 30
+      echo "*** Service started with PID ${SERVICE_PID}. Waiting for 60 seconds..."
+      sleep 60
       NEW_PID=$(get_port_pid)
       if [ -n "$NEW_PID" ] && [ "$NEW_PID" -eq "$SERVICE_PID" ]; then
-        echo "Service successfully started and running with PID ${SERVICE_PID}."
-        return
+        echo "*** Service successfully started and running with PID ${SERVICE_PID}."
+        break
       else
-        echo "Service failed to start. Retrying..."
+        echo "*** Service failed to start. Retrying..."
       fi
     else
-      echo "Port ${SERVICE_PORT} is in use by PID ${PID}. Killing process..."
+      echo "*** Port ${SERVICE_PORT} is in use by PID ${PID}. Killing process..."
       kill -9 $PID
       sleep 60  # 确保端口释放
     fi
@@ -39,11 +38,25 @@ start_service() {
 monitor_service() {
   start_service
   while true; do
-    sleep ${RESTART_INTERVAL}
-    echo "Restarting service..."
-    kill ${SERVICE_PID}
-    wait ${SERVICE_PID} 2>/dev/null
-	start_service
+	# 获取当前分钟数
+	current_minute=$(date +%M)
+	# 检查是否为整点（00分）或30分
+	# if [ "$current_minute" = "00" ] || [ "$current_minute" = "30" ] || [ "$current_minute" = "20" ]  || [ "$current_minute" = "10" ]  || [ "$current_minute" = "40" ]  || [ "$current_minute" = "50" ] ; then
+	if [ "$current_minute" = "00" ] || [ "$current_minute" = "30" ] ; then
+		formatted_date=$(date '+%Y年%m月%d日 %H时%M分%S秒')
+		echo " "
+		echo " "
+		echo " "
+		echo " "
+		echo " "
+		echo "*** $formatted_date  ---  当前时间是整点或30分"
+		echo "*** Restarting service..."
+		PID=$(get_port_pid)
+		kill -9 ${PID}
+		wait ${PID} 2>/dev/null
+		sleep 60
+		start_service
+	fi
   done
 }
 
